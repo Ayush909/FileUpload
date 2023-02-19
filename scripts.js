6d43dc65b9fb681adf05ee1c864212505cd01800
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2023-02-19 21:18:30 +0530

    adding client side code

---
+++ scripts.js
@@ -0,0 +1,41 @@
+const myform = document.getElementById("myform");
+const fileInput = document.getElementById("myimg");
+
+function submitHandler(e) {
+  e.preventDefault();
+  const file = fileInput.files[0];
+
+  if (!file) {
+    alert("Please choose a file");
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.readAsBinaryString(file);
+
+  reader.onload = async function () {
+    const binaryString = reader.result;
+    const base64String = btoa(binaryString);
+
+    // Send the base64String to the server using an HTTP request
+    console.log(base64String);
+    const url = "http://localhost:8000/upload";
+    const body = base64String;
+
+    const options = {
+      method: "POST",
+      body: body,
+      redirect: "manual",
+    };
+
+    const response = await fetch(url, options);
+
+    if (response.ok) {
+      console.log("Image uploaded successfully");
+    } else {
+      console.error("Error uploading image");
+    }
+  };
+}
+
+myform.addEventListener("submit", (e) => submitHandler(e));
