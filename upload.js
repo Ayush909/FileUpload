d7a3343fe74f362b857f3b5a83efe5845d86a167
Ayush Singh Kushwaha <50234003+Ayush909@users.noreply.github.com>
2023-02-19 21:16:12 +0530

    server side code

---
+++ upload.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require("fs");
+
+function setCorsHeaders(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  console.log(req.headers);
+  next();
+}
+
+function myapi(req, res) {
+  if (req.url === "/upload") {
+    let imageData = "";
+    req.on("data", (chunck) => {
+      imageData += chunck;
+    });
+
+    req.on("end", () => {
+      const fileName = Math.random() + "my-image.png";
+      const filePath = "./uploads/" + fileName;
+
+      fs.writeFile(filePath, imageData, "base64", function (err) {
+        if (err) {
+          console.log(err);
+          res.statusCode = 500;
+          res.end("Error saving image");
+        } else {
+          res.statusCode = 200;
+          res.end("Image saved");
+        }
+      });
+    });
+  }
+}
+
+http
+  .createServer((req, res) => {
+    setCorsHeaders(req, res, () => {
+      myapi(req, res);
+    });
+  })
+  .listen(8000, () => {
+    console.log("Server runnning on port 8000");
+  });
